75a8221a2827ef117264c6f83622e4cb46280351
William Cruz <wi.cruzm@gmail.com>
2019-02-19 22:58:37 +0800

    Addition to Ch2
    New figure and R script

--- stats_Design&Multivariate.R
+++ stats_Design&Multivariate.R
@@ -62,3 +62,32 @@ pd <- position_dodge(width = 0.2)
       axis.line.y = element_line(colour = "black", size = 1)
     )
 )
+##################################
+## PART 2 Statistical Inference ##
+##################################
+# Figure on the Beta distribution (Type II Error)
+par(mfrow=c(3,1)) ; 
+n = 300
+x1 <- rnorm(n,mean = 19.5, sd=0.4)
+h1 <- hist(x1,breaks = 1, xlim = c(17,21),ylim=c(0,700),
+           border = "white",main="",xlab = expression(mu == 19.5),ylab = "") 
+x1fit <- seq(min(x1),max(x1),length=length(x1)) ; abline(v=19.5,lwd=3) ; abline(v=18.842)
+y1fit <- dnorm(x1fit,mean=mean(x1),sd=sd(x1)) ; y1fit <- y1fit*diff(h1$mids[1:2])*n
+lines(x1fit,y1fit,col="blue", lwd=2)
+text(18,150,expression(alpha == 0.05)) ; text(21,100,expression(bar(y)))
+
+x2 <- rnorm(n,mean = 19, sd=0.4)
+h2 <- hist(x2,breaks = 1, xlim = c(17,21),ylim=c(0,700),
+           border = "white",main="",xlab = expression(mu == 19.0),ylab = "") 
+x2fit <- seq(min(x2),max(x2),length=length(x2)) ; abline(v=19.0,lwd=3) ; abline(v=18.842)
+y2fit <- dnorm(x2fit,mean=mean(x2),sd=sd(x2)) ; y2fit <- y2fit*diff(h2$mids[1:2])*n
+lines(x2fit,y2fit,col="blue", lwd=2)
+text(20,600,expression(beta)) ; text(21,100,expression(bar(y)))
+
+x3 <- rnorm(n,mean = 18.5, sd=0.4)
+h3 <- hist(x3,breaks = 1, xlim = c(17,21),ylim=c(0,700),
+           border = "white",main="",xlab = expression(mu == 18.5),ylab = "") 
+x3fit <- seq(min(x3),max(x3),length=length(x3)) ; abline(v=18.5,lwd=3) ; abline(v=18.842)
+y3fit <- dnorm(x3fit,mean=mean(x3),sd=sd(x3)) ; y3fit <- y3fit*diff(h3$mids[1:2])*n
+lines(x3fit,y3fit,col="blue", lwd=2)
+text(19.5,400,expression(beta)) ; text(21,100,expression(bar(y)))
